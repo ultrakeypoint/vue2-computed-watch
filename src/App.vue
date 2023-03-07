6f8af105bae57f2f657d20628b3cdc91ef49bf7d
ultrakeypoint <73173310+ultrakeypoint@users.noreply.github.com>
2023-03-08 00:02:44 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,6 @@
     <p>Computed reversed message: "{{ reversedMessage }}"</p>
     <p>Computed reversed message: "{{ this.message.split('').reverse().join('') }}"</p>
     <p>Computed reversed watched: "{{ watched }}"</p>
-    <p>Computed reversed message: "{{ this.test() }}"</p>
-    <p>Computed reversed message: "{{ this.test() }}"</p>
-    <p>Computed reversed message: "{{ this.test() }}"</p>
 
     <button @click="update()">update</button>
 
@@ -21,31 +18,51 @@ export default {
     return {
       updatecount: 0,
       message: '오늘은 무엇을 먹을까요?',
+      message2: '2',
+      message3: '3',
       watched: '',
     }
   },
-  
+
   computed: {
-    reversedMessage: function () {
-      return this.message.split('').reverse().join('')
-    },
+    // reversedMessage: function () {
+    //   ㅁㄴㅇㄹ
+    //   return this.message.split('').reverse().join('')
+    // },
     // reversedMessage: async function () {
     //   this.variable = '1234'
     //   const r = await this.resolveAfter2seconds(2)
     //   return this.message.split('').reverse().join('')
     // },
+    // reversedMessage: function () {
+    //   // this.resolveAfter2seconds(2)
+
+    //   // 1
+    //   // 2
+    //   // 3
+
+    //   return this.message.split('').reverse().join('')
+    // },
+    reversedMessage: function () {
+      // this.message = '1'
+      // this.message2 = '2'
+      this.message = this.message3
+
+      return this.message2 + this.message
+    },
   },
 
   watch: {
-    reversedMessage: async function (n, o) {
-      const r = await this.resolveAfter2seconds(2)
+    reversedMessage: function (n, o) {
+      const r = this.resolveAfter2seconds(2)
       this.watched = n + r
     },
   },
 
   methods: {
     async update() {
-      this.message = this.message + ++this.updatecount
+      // this.message = this.message + ++this.updatecount
+      this.message3 = this.message3 + '1'
     },
     resolveAfter2seconds(x) {
       console.log('resolveAfter10second')
@@ -55,12 +72,12 @@ export default {
         }, 2000)
       })
     },
-    test() {
-      console.log(1)
-      return 1
-    },
+
+    // selectlist 가져오는 거다.
+    // getSelectList
     getSelectList() {
       this.variable = 'test'
+      //
       return [1, 2, 3, 4, 5]
     },
   },
