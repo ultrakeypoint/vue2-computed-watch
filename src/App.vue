0cb504faa39fffa13d67b7e68a280a25dd40d1fe
ultrakeypoint <73173310+ultrakeypoint@users.noreply.github.com>
2023-03-08 10:58:25 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="example">
-    <p>updatecount: "{{ updatecount }}"</p>
     <p>Original message: "{{ message }}"</p>
-    <p>Computed reversed message: "{{ reversedMessage }}"</p>
-    <p>Computed reversed watched: "{{ loadingWatch }}"</p>
+    <p>Computed message: "{{ requestMessage }}"</p>
+    <p>watched message: "{{ watchedMessage }}"</p>
 
     <button @click="update()">update</button>
   </div>
@@ -14,37 +13,30 @@ export default {
   data() {
     return {
       updatecount: 0,
-      message: '오늘은 무엇을 먹을까요?',
+      message: '뭐 먹을까요?',
       watchedMessage: '',
-
-      myMoney: 0,
-      debt: 0,
-
       loading: false,
     }
   },
 
   computed: {
-    reversedMessage: function () {
-      return this.message.split('').reverse().join('')
-    },
-    loadingWatch: function () {
-      return this.loading ? '생각중' : this.watchedMessage
+    requestMessage: function () {
+      return '점심 ' + this.message
     },
   },
 
   watch: {
-    reversedMessage: async function (n, o) {
+    requestMessage: async function (n, o) {
       this.loading = true
       const r = await this.resolveAfter2seconds(2)
-      this.watchedMessage = n + ++this.updatecount + r
+      this.watchedMessage = n + ++this.updatecount
       this.loading = false
     },
   },
 
   methods: {
     async update() {
-      this.message = this.message + '?'
+      this.requestMessage = this.requestMessage + '(' + Math.random() + ')'
     },
     resolveAfter2seconds(x) {
       console.log('resolveAfter10second')
@@ -53,16 +45,6 @@ export default {
           resolve(' - 밥' + this.updatecount)
         }, 2000)
       })
-    },
-
-    // selectlist 가져오는 거다.
-    // getSelectList
-    increaseDebt(money) {
-      this.debt = money
-    },
-    getMyMoney() {
-      // this.myDebt = this.myDebt + 100
-      return this.myMoney
     },
   },
 }
